56d410cc9b3c1bdfc2121e90c9ef9df8ebf878f3
tsungtingdu <tsungtingdu@gmail.com>
2019-12-24 13:55:14 +0800

    init project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
+
+// setup view engine
+app.engine('.hbs', handlebars({
+  extname: '.hbs',
+  defaultLayout: 'main',
+  partialsDir: __dirname + '/views/partials',
+  helpers: require('./config/handlebars-helpers')
+}))
+
+app.set('view engine', '.hbs')
+app, use(express.static('public'))
+
+// listening on port
+app.listen(port, () => {
+  console.log(`Listening on port ${port} ...`)
+})
